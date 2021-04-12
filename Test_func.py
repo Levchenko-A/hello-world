b856a353c8099e386a58f70c9803f98f0bb2a628
Andrii Levchenko <82201855+Levchenko-A@users.noreply.github.com>
2021-04-12 09:08:20 +0300

    making some changes

--- Test_func.py
+++ Test_func.py
@@ -3,6 +3,6 @@ def test(a):
         and up to "a"'''
     b = [i ** 2 for i in range(a)]
     return b
-#string added to test branching
+#string added to test branching and merging
 print('This is a new string in the file')
 print(test(9))
